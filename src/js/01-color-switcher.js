9098d9b543fa949a458c7bec2dbbfbb79782de6f
soberjan1 <aliguseynov48@gmail.com>
2023-05-17 17:06:10 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  bodyColor: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stoptBtn: document.querySelector('button[data-stop]'),
+};
 
+let intervalId = null;
+const PROMPT_DELAY = 1000;
+const colorChangeFn = function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+refs.startBtn.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = colorChangeFn();
+  }, PROMPT_DELAY);
+
+  if (intervalId) {
+    refs.startBtn.disabled = true;
+  }
+});
+
+refs.stoptBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  refs.startBtn.disabled = false;
+});
