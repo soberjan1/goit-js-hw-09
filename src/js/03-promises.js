dc89b08826a7eb648aacd4cbab3ede0d9117fb23
soberjan1 <aliguseynov48@gmail.com>
2023-05-21 20:58:41 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,54 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+Notify.init({
+  width: '280px',
+  position: 'right-top',
+  distance: '10px',
+  success: {
+    background: '#32c682',
+    textColor: '#fff',
+  },
+
+  failure: {
+    background: '#ff5549',
+    textColor: '#fff',
+  },
+});
+
+const refs = {
+  formEl: document.querySelector('.form'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+refs.formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+
+  const { delay, step, amount } = event.target.elements;
+  let delayEl = parseInt(delay.value);
+  let stepEl = parseInt(step.value);
+
+  for (let i = 0; i <= amount.value; i++) {
+    createPromise(i, delayEl)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayEl += stepEl;
   }
 }
